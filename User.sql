e74f2abf3747f93607b32ae9611dc4e2e1d872c3
Edziel-Tr <elohera_240000001068@uic.edu.ph>
2025-09-18 14:02:54 +0800

    Update User.sql

--- User.sql
+++ User.sql
@@ -1,7 +1,12 @@
 CREATE DATABASE Users;
 USE Users;
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(255) NOT NULL,
+    Password VARCHAR(255) NOT NULL
+);
+
 INSERT INTO users (Username, password)
 VALUES ('Default User', 'user', '123');
 
-);
